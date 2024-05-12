79bb4a3beec5ae809dc49cfaf1a68597ea0479be
mariyumsohail <mariyumsohail71@gmail.com>
2024-05-12 23:41:44 +0500

    index.html

---
+++ chapter4/app.js
@@ -0,0 +1,10 @@
+
+document.write("<h1>legall variable names</h1>");
+document.write("my_variable ,userAge ,total_sum ,num1, _data");
+document.write("<h1>ilegall variable names</h1>");
+document.write("1st_variable (Starting with a digit) \n ,user-age (Containing a hyphen) \n ,total sum (Containing a space) \n, @data (Starting with a special character)  ,\n for (Using a reserved keyword)");
+document.write("<h1>“Rules for naming JS variables”</h1>");
+document.write("b) Variable names can only contain number, $, and _ for example : $my _1stvariable")
+document.write("c) Variables must begin with a letter , $ or _ For example $name, _name or name")
+document.write("d) Variable names are case sensitive") 
+document.write("d) Variable names should not be js variables")
---
+++ chapter5/app.js
@@ -0,0 +1,48 @@
+//subtraction//
+var num = 10 ;
+console.log( "20" - num)
+//addition//
+var num = 10 ;
+console.log( 20 + num)
+//multiplication//
+var num = 100 ;
+console.log( "200" * num)
+//division//
+var num = 100 ;
+console.log( "200" / num)
+//modulous//
+var num = 800 ;
+console.log( "400" % num)
+
+//Do the following using JS Mathematic Expressions
+//a Declare a variable//
+var number ;
+document.write( "Value after variable declaration is:" + number + "<br>" )
+number = 5 ;
+console.log(number)
+document.write( "initial value" + number + "<br>");
+number++
+console.log(number);
+document.write("value after increment is" + number  + "<br>" );
+number += 7;
+document.write("Value after addition is: " + number + "<br>");
+number--
+console.log(number);
+document.write("Value after decrement is: " + number + "<br>");
+console.log( number)
+document.write("Value after decrement is: " +  number + "<br>");
+var remainder = number % 3;
+document.write("The remainder is: " + remainder);
+
+//Cost of one movie ticket is 600 PKR. Write a script to store//
+//ticket price in a variable & calculate the cost of buying 5 tickets to a movie. Example output://
+var ticketPrice = 600;
+var totalCost = ticketPrice *5;
+document.write("total cost to buy 5 tickets to a movie is " + totalCost);
+//5. Write a script to display multiplication table of any number in your browser.//
+document.write("<h1>TABLE OF 4</h1>")
+var number = 4;
+var multiply = 
+var num = 1 ; 
+var equals = 
+document.write("")
